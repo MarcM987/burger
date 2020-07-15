3843efbb0dc24e283485d3b1ed818150e22db5b6
Marc_M <marcmartinez987@gmail.com>
2020-07-14 19:50:30 -0700

    initial app and db setup completed, hierarchy and db created

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger_name, dervoured)
+VALUES ("BIGbigBurger", TRUE);
+INSERT INTO burgers(burger_name, dervoured)
+VALUES ("BIGsmallBurger", TRUE);
+INSERT INTO burgers(burger_name, dervoured)
+VALUES ("SMALLsmallBurger", FALSE);
