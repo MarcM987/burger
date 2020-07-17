1fe90346bb5689e5748319d1f930515aa479ca99
MarcM987 <marcmartinez1@192.168.1.11>
2020-07-16 21:56:08 -0700

    more tests

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 var mysql = require("mysql");
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Marc987987!!12",
-    database: "burgers_db"
-  });
-}
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+  // connection = mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   password: "Marc987987!!12",
+  //   database: "burgers_db"
+  // });
+// }
+
+connection = mysql.createConnection({
+  host: "x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "iiz14aqw3sewuuzt",
+  password: "jqua32gg026oehzm",
+  database: "xa24yal39g5l9jc5"
+});
 
 // Make connection.
 connection.connect(function(err) {
